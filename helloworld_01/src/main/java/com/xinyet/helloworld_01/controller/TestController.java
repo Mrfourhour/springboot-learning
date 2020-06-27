3d2d3bfd6c768838b3800b69ec20cec5b90ca113
xinyet <xinyet@aliyun.com>
2020-06-27 09:43:27 +0800

    测试更新

--- helloworld_01/src/main/java/com/xinyet/helloworld_01/controller/TestController.java
+++ helloworld_01/src/main/java/com/xinyet/helloworld_01/controller/TestController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller /*声明这是个controller并将其托管给spring */
 public class TestController {
-
     @ResponseBody/*@ResponseBody 表示该方法的返回结果直接写入 HTTP response body 中，通常用来返回JSON数据或者是XML数据。*/
     @RequestMapping("/hello")/*请求路径*/
     public String hello(){
